68c2f08e414414551595a61b9264fa5ebe5bdf04
Oleksii <zankexd@icloud.com>
2022-07-12 17:30:53 +0200

    Finish

---
+++ HomeWork1/Task1.py
@@ -0,0 +1,7 @@
+N=int(input("Please type number if you want to now date of weekend "))
+if 0<N<=5:
+    print("no")
+elif 7>N>5:
+    print("yes")
+else:
+    print("Sorry,try next number")
---
+++ HomeWork1/Task4.py
@@ -0,0 +1,6 @@
+
+X=int(input())
+X1=int(input())
+Y1=int(input())
+Y=int(input())
+print((((X-X1)**2)+((Y-Y1)**2))**(0.5))
